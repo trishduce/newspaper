f147b4d1690fbae1901ac0b10186ce177b0c07ec
Trish Duce <ducepa@mso.umt.edu>
2024-11-26 10:06:47 -0700

    updated settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,11 +25,7 @@ CSRF_TRUSTED_ORIGINS = [
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-FORCE_SCRIPT_NAME = (
-    "/" + os.environ.get("SITE_NAME", "")
-    if os.environ.get("SITE_NAME", "") != ""
-    else ""
-)
+FORCE_SCRIPT_NAME = "/" + os.environ.get("SITE_NAME", "")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
